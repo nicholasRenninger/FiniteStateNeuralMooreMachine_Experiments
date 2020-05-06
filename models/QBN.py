8bbdc0b6acdb7e532e8e913e713bc964418355df
Nicholas Renninger <18428974+nicholasRenninger@users.noreply.github.com>
2020-05-06 07:00:11 -0600

    OQBN is super well-trained. ouch tf1 is hard

--- models/QBN.py
+++ models/QBN.py
@@ -1,5 +1,8 @@
-import numpy as np
 import tensorflow as tf
+from stable_baselines.common.base_class import TensorboardWriter
+from stable_baselines.common.tf_util import make_session
+from contextlib import suppress
+import math
 
 if __name__ == "__main__":
     import sklearn.preprocessing as prep
@@ -17,7 +20,7 @@ else:
 
 class QBN(object):
     """
-    A quantized bottleneck network, which is really a quntizing
+    A quantized bottleneck network, which is really a quantizing
     autoencoder in disguise™
 
     This QBN reduces the dimensionality and quantizes its input features into a
@@ -39,53 +42,132 @@ class QBN(object):
                                              function. This should be quantized
                                              so that the latent state is
                                              quantized.
-    :param      optimizer:                   The optimizer
+    :param      decode_activation_function   activation function on the final
+                                             layer of the QBN which is supposed
+                                             to be the reconstructed input
+    :param      optimizer:                   The training optimizer
+    :param      gradient_clip_val:           global norm to clip all gradients
     :param      loss_func:                   The loss function for training
-    :param      log_dir:                     The tensorboard logging directory
     """
 
-    def __init__(self,
-                 layer_sizes_by_input_scaling_factors=None,
-                 latent_size=None,
-                 layer_sizes=None,
+    def __init__(self, layer_sizes,
                  layers_activation_function=tf.nn.tanh,
                  latent_activation_function=n_ary_activation,
+                 decode_activation_function=tf.nn.relu6,
                  optimizer=tf.train.AdamOptimizer(),
+                 gradient_clip_val=5.0,
                  loss_func=tf.compat.v1.losses.mean_squared_error,
-                 log_dir='logs'):
+                 net_name='qbn', sess=None):
 
-        self.input_dim = self.layer_sizes[0]
-
-        has_layers = layer_sizes_by_input_scaling_factors is not None
-        has_latent = latent_size is not None
-        proper_net_spec = (has_latent and has_layers)
-        if layer_sizes is not None:
-            self.layer_sizes = layer_sizes
-        elif proper_net_spec:
-            self.layer_sizes = [scaling * self.input_dim for scaling in
-                                layer_sizes_by_input_scaling_factors]
-            self.layer_sizes.append(latent_size)
+        self.graph = tf.Graph()
+        if sess is None:
+            self.sess = tf.Session()
         else:
-            msg = 'must supply a non-None value for both ' + \
-                  'layer_sizes_by_input_scaling_factors ' + \
-                  f'({layer_sizes_by_input_scaling_factors}) and ' + \
-                  f'latent_size ({latent_size})'
-            ValueError(msg)
+            self.sess = make_session(graph=self.graph)
 
-        self.hidd_activation = layers_activation_function
-        self.latent_activation = latent_activation_function
+        self.net_name = net_name
+        with self.graph.as_default():
 
-        network_weights = self._initialize_weights()
-        self.weights = network_weights
+            self.hidd_activation = layers_activation_function
+            self.latent_activation = latent_activation_function
+            self.reconstruction_activation = decode_activation_function
 
-        self.x = tf.placeholder(tf.float32, [None, self.input_dim])
-        self.x_test = tf.placeholder(tf.float32, [None, self.input_dim])
+            self.layer_sizes = layer_sizes
+            self.input_dim = self.layer_sizes[0]
 
-        # use x as the variable we encode and then decode with the
-        # encoder / decoder modules
-        h = self.x
+            self.grad_clip_val = gradient_clip_val
 
-        # build encoder module
+            network_weights = self._initialize_weights()
+            self.weights = network_weights
+
+            self.x = tf.placeholder(tf.float32, [None, self.input_dim])
+
+            # use x as the variable we encode and then decode with the
+            # encoder / decoder modules
+            h = self.x
+
+            # use encoder module
+            self.encoding = self._encode_ops(h)
+
+            # use the decoder module
+            self.decoding = self._decode_ops(self.encoding)
+
+            with tf.variable_scope('train', reuse=tf.AUTO_REUSE):
+
+                # MSE Reconstruction Loss / loss
+                self.train_loss = loss_func(self.x, self.decoding)
+                self.train_loss_summ = tf.summary.scalar('Train_Loss',
+                                                         self.train_loss)
+
+                # need to clip gradients for training stability
+                self.optimizer = optimizer
+                grad_info = self.optimizer.compute_gradients(self.train_loss)
+                self.gradients, self.variables = zip(*grad_info)
+                self.gradients, _ = tf.clip_by_global_norm(self.gradients,
+                                                           self.grad_clip_val)
+                clipped_grads = zip(self.gradients, self.variables)
+                self.optimize = self.optimizer.apply_gradients(clipped_grads)
+
+            tf.global_variables_initializer().run(session=self.sess)
+
+    def _initialize_weights(self):
+        all_weights = dict()
+        initializer = tf.contrib.layers.xavier_initializer()
+
+        with tf.variable_scope(self.net_name, reuse=tf.AUTO_REUSE):
+
+            # Encoding network weights
+            with tf.variable_scope('encode', reuse=tf.AUTO_REUSE):
+
+                encoder_weights = []
+                for layer in range(len(self.layer_sizes) - 1):
+
+                    # layer weight tensor
+                    init_w = initializer((self.layer_sizes[layer],
+                                          self.layer_sizes[layer + 1]),
+                                         dtype=tf.float32)
+                    w = tf.get_variable(f'w{layer}', initializer=init_w)
+
+                    # layer bias tensor
+                    init_b = tf.zeros([self.layer_sizes[layer + 1]],
+                                      dtype=tf.float32)
+                    b = tf.get_variable(f'b{layer}', initializer=init_b)
+
+                    encoder_weights.append({'w': w, 'b': b})
+
+            # decoder network weights
+            with tf.variable_scope('decode', reuse=tf.AUTO_REUSE):
+
+                decoder_weights = []
+                for layer in range(len(self.layer_sizes) - 1, 0, -1):
+
+                    # layer weight tensor
+                    init_w = initializer((self.layer_sizes[layer],
+                                          self.layer_sizes[layer - 1]),
+                                         dtype=tf.float32)
+                    w = tf.get_variable(f'w{layer}', initializer=init_w)
+
+                    # layer bias tensor
+                    init_b = tf.zeros([self.layer_sizes[layer - 1]],
+                                      dtype=tf.float32)
+                    b = tf.get_variable(f'b{layer}', initializer=init_b)
+
+                    decoder_weights.append({'w': w, 'b': b})
+
+            all_weights['encode'] = encoder_weights
+            all_weights['decode'] = decoder_weights
+
+        return all_weights
+
+    def _encode_ops(self, x):
+        """
+        runs ops for encoder module.
+
+        returns the encoded latent state for the network input x
+        """
+
+        h = x
+
         self.encode_layer_tensors = []
         latent_layer_idx = len(self.layer_sizes) - 2
         for layer in range(len(self.layer_sizes) - 1):
@@ -101,82 +183,54 @@ class QBN(object):
                              self.weights['encode'][layer]['b']))
             self.encode_layer_tensors.append(h)
 
-        self.encoding = self.encode_layer_tensors[-1]
+        return self.encode_layer_tensors[-1]
 
-        # build decoder module
+    def _decode_ops(self, h):
+        """
+        runs ops for decoder module.
+
+        returns the decoded version of the latent state h
+        """
+
         self.decode_layer_tensors = []
+        reconstruction_layer_idx = len(self.layer_sizes) - 2
         for layer in range(len(self.layer_sizes) - 1):
-            h = self.hidd_activation(
-                tf.add(tf.matmul(h, self.weights['decode'][layer]['w']),
-                       self.weights['decode'][layer]['b']))
+
+            # reconstruction (final) layer needs a relu6 as per the paper
+            if layer == reconstruction_layer_idx:
+                activ = self.reconstruction_activation
+            else:
+                activ = self.hidd_activation
+
+            h = activ(tf.add(tf.matmul(h, self.weights['decode'][layer]['w']),
+                             self.weights['decode'][layer]['b']))
             self.decode_layer_tensors.append(h)
 
-        self.decoding = self.decode_layer_tensors[-1]
-
-        # MSE Reconstruction Loss / loss
-        self.train_loss = loss_func(self.x, self.decoding)
-        self.test_loss = loss_func(self.x_test, self.decoding)
-
-        # merge all summaries :)
-        self.train_loss_summ = tf.summary.scalar('Train Loss', self.train_loss)
-        self.test_loss_summ = tf.summary.scalar('Test Loss', self.test_loss)
-        self.merged_summ = tf.compat.v1.summary.merge_all()
-
-        self.optimizer = optimizer.minimize(self.train_loss)
-
-        init = tf.global_variables_initializer()
-        self.sess = tf.Session()
-        self.sess.run(init)
-
-    def _initialize_weights(self):
-        all_weights = dict()
-        initializer = tf.contrib.layers.xavier_initializer()
-
-        # Encoding network weights
-        encoder_weights = []
-        for layer in range(len(self.layer_sizes) - 1):
-            w = tf.Variable(initializer((self.layer_sizes[layer],
-                                         self.layer_sizes[layer + 1]),
-                                        dtype=tf.float32))
-            b = tf.Variable(
-                tf.zeros([self.layer_sizes[layer + 1]], dtype=tf.float32))
-            encoder_weights.append({'w': w, 'b': b})
-
-        # decoder network weights
-        recon_weights = []
-        for layer in range(len(self.layer_sizes) - 1, 0, -1):
-            w = tf.Variable(initializer((self.layer_sizes[layer],
-                                         self.layer_sizes[layer - 1]),
-                                        dtype=tf.float32))
-            b = tf.Variable(
-                tf.zeros([self.layer_sizes[layer - 1]], dtype=tf.float32))
-            recon_weights.append({'w': w, 'b': b})
-
-        all_weights['encode'] = encoder_weights
-        all_weights['decode'] = recon_weights
-
-        return all_weights
+        return self.decode_layer_tensors[-1]
 
     def partial_fit(self, train_data, test_data):
         """
         does a batch of gradient descent
 
-        :param      train_data:  The training data
-        :param      test_data:   The test data to evaluate the model on
+        :param      train_data:    The training data
+        :param      test_data:     The test data to evaluate the model on
 
-        :returns:   losses and summaries for tb writing
-                    (train_loss, test_loss, merged_summaries)
+        :returns:   losses and tb summaries
+                    (train_loss, test_loss, train_loss_summ, test_loss_summ)
         """
 
-        (train_loss, test_loss, opt,
-         merged_summaries) = self.sess.run([self.train_loss,
-                                            self.test_loss,
-                                            self.optimizer,
-                                            self.merged_summ],
-                                           feed_dict={self.x: train_data,
-                                                      self.x_test: test_data})
+        (train_loss, opt,
+         train_loss_summ) = self.sess.run([self.train_loss,
+                                           self.optimize,
+                                           self.train_loss_summ],
+                                          feed_dict={self.x: train_data})
 
-        return train_loss, test_loss, merged_summaries
+        (test_loss,
+         test_loss_summ) = self.sess.run([self.train_loss,
+                                          self.train_loss_summ],
+                                         feed_dict={self.x: test_data})
+
+        return train_loss, test_loss, train_loss_summ, test_loss_summ
 
     def fit(self, X, X_test,
             training_epochs=400, batch_size=32, n_samples=None,
@@ -198,47 +252,48 @@ class QBN(object):
         """
 
         write_to_logs = (log_dir is not None)
-
-        # for tensorboard logging of training
         if write_to_logs:
-            writer = tf.compat.v1.summary.FileWriter(log_dir)
+            tbw_train = TensorboardWriter(self.graph, log_dir, 'train')
+            tbw_test = TensorboardWriter(self.graph, log_dir, 'test')
+        else:
+            tbw_train = suppress()
+            tbw_test = suppress()
 
         # try to compute the number of samples if not given, but sometimes
         # len is not going to work.
         if n_samples is None:
             n_samples = len(X)
 
-        for epoch in range(training_epochs):
+        with tbw_test as test_writer, tbw_train as train_writer:
+            for epoch in range(training_epochs):
 
-            total_batch = int(n_samples / batch_size)
+                num_batches = math.ceil(n_samples / batch_size)
 
-            # Loop over all batches
-            for i in range(total_batch):
+                # Loop over all batches
+                for i in range(num_batches):
 
-                batch_xs = self.get_random_block_from_data(X, batch_size)
-                batch_xs_test = self.get_random_block_from_data(X_test,
-                                                                batch_size)
+                    batch_start_idx = (i * batch_size)
+                    batch_end_idx = (i * batch_size) + batch_size
+                    batch_xs = X[batch_start_idx:batch_end_idx]
+                    batch_xs_tst = X_test[batch_start_idx:batch_end_idx]
 
-                # Fit training using batch data
-                (train_loss, test_loss,
-                 merged_summaries) = self.partial_fit(train_data=batch_xs,
-                                                      test_data=batch_xs_test)
+                    # Fit training using batch data
+                    (train_loss, test_loss,
+                     train_loss_summ,
+                     test_loss_summ) = self.partial_fit(train_data=batch_xs,
+                                                        test_data=batch_xs_tst)
 
-            # update tensorboard logs with the loss from the most recent
-            # batch of data
-            if write_to_logs:
-                writer.add_summary(merged_summaries, epoch)
+                # update tensorboard logs with the loss from the most recent
+                # batch of data
+                if write_to_logs:
+                    train_writer.add_summary(train_loss_summ, epoch)
+                    test_writer.add_summary(test_loss_summ, epoch)
 
-            # Display logs per epoch step
-            if (epoch % display_step == 0) and verbose:
-                print("Epoch:", '%d,' % (epoch + 1),
-                      "train_loss:", "{:.9f}".format(train_loss),
-                      "test_loss:", "{:.9f}".format(test_loss))
-
-    def get_random_block_from_data(self, data, batch_size):
-
-        start_index = np.random.randint(0, len(data) - batch_size)
-        return data[start_index:(start_index + batch_size)]
+                # Display logs per epoch step
+                if (epoch % display_step == 0) and verbose:
+                    print("Epoch:", '%d,' % (epoch + 1),
+                          "train_loss:", "{:.9f}".format(train_loss),
+                          "test_loss:", "{:.9f}".format(test_loss))
 
     def format_data(self, data):
         data_shape = data.shape
